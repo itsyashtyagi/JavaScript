e955745916cd4805d27299a064f34b89789bece3
Yash Tyagi <94339265+itsyashtyagi@users.noreply.github.com>
2024-06-20 05:00:00 +0000

    variables and datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all JS code as a newer version of JS
+
+// alert(3 + 3) // we are using the node js not the browser
+
+let name = "yash" //string
+let age = 18 // number, bigint
+let isLoggedIn = true //boolean
+let state; 
+
+// string, number, bigint, boolean, symbol(unique character), null (standalone value), undefined (it means the value is not assigned yet), object
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof state);
+console.log(typeof null); 
+console.log(typeof undefined); 
+console.log(typeof Object);
---
+++ 01_basics/variables.js
@@ -0,0 +1,19 @@
+console.log("yash tyagi")
+var accountId = 12354
+let accountEmail = "yashtyagi@google.com"
+const accountPassword = 1967
+accountCity = "sambhal"
+
+// accountPassword = 2345 // not allowed 
+
+accountId = 45678
+accountEmail = "yash@google.com" 
+accountCity = "ghaziabad"
+
+/* 
+Prefer not to use the var beacuse 
+of the scope and functional issue in that
+*/
+
+console.log(accountId); 
+console.table([accountId, accountEmail, accountPassword, accountCity]);
