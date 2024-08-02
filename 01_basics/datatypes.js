fb8ae2240a9ee2ebba1921778bb352d119f9140d
Yash Tyagi <94339265+itsyashtyagi@users.noreply.github.com>
2024-08-02 04:39:50 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all JS code as a newer version
+
+// alert( 3 + 3 ) // Not allowed because we are in nodejs not in browser
+
+let name = "yash" // string
+let age = 10 // number, bigint
+let isLoggedIn = true // boolean
+let city = null; // null is a standalone value
+let country; // undefined (means not assigned any value yet)
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
