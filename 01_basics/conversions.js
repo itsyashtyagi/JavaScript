5ddfa287699dfc699a4dedd13c2df0b7f8e7c16e
Yash Tyagi <94339265+itsyashtyagi@users.noreply.github.com>
2024-08-03 19:09:56 +0000

    conversion of datatypes

---
+++ 01_basics/conversions.js
@@ -0,0 +1,33 @@
+let score = undefined
+
+let scoreInNumber = Number(score)
+// console.log(typeof scoreInNumber);
+// console.log(scoreInNumber);
+
+// "yash" => NaN
+// 24 => 24
+// true => 1; false => 0
+// "" => 0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = undefined
+
+let boolenIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof boolenIsLoggedIn);
+// console.log(boolenIsLoggedIn);
+
+// 123 => true
+// 0 => false
+// "yash" => true
+// "" => false
+// null => false
+// undefined => false
+
+let someNumber = undefined
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
+// 123 => "123"
