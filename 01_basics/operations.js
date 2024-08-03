534f3cb15418b25fab2deab08851960a91fadb96
Yash Tyagi <94339265+itsyashtyagi@users.noreply.github.com>
2024-08-03 19:17:30 +0000

    coversions

---
+++ 01_basics/operations.js
@@ -0,0 +1,38 @@
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+// console.log(2+3);
+// console.log(4-3);
+// console.log(2 * 3);
+// console.log(3 / 6);
+// console.log(3 % 6);
+// console.log(3 ** 2);
+
+let str1 = "hello"
+let str2 = " yash"
+let str3 = str1 + str2
+// console.log(str3);
+
+console.log("1" + 2);
+console.log("1" + "2" + 3);
+console.log("1" + 2 + "3");
+console.log("1" + 2 + 3);
+console.log(1 + 2 + "3");
+
+let gameCounter = 100
+++gameCounter
+gameCounter--
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+
+
+
+
+
+
+
