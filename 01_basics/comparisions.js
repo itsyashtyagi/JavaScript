a785be2d4475d0cbd4f47528c1cbaf8aeba87e6d
Yash Tyagi <94339265+itsyashtyagi@users.noreply.github.com>
2024-08-03 19:30:42 +0000

    comparision

---
+++ 01_basics/comparisions.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 <= 1);
+// console.log(2 >= 1);
+// console.log( 2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+// === (use for strict chceck in which it check the value as well as the datatype of it)
+
+console.log("02" === 2);
+
+
