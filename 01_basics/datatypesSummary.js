532a4bad9576462a68a256e0dae8d618a87153b3
Yash Tyagi <94339265+itsyashtyagi@users.noreply.github.com>
2024-08-08 03:45:21 +0000

    datatyapes-summary

---
+++ 01_basics/datatypesSummary.js
@@ -0,0 +1,21 @@
+// Prmitive
+
+// 7 types - String, Number, Boolean, BigInt, Symbol, null, undefined
+
+
+// Reference type ( Non - Primitive type)
+
+// Array, Objects, Functions
+
+const names = ["yash", "raghav", "rahul"]
+
+const myObj = {
+    name : "yash", 
+    age : 21,
+}
+
+const myFunction = function(){
+    console.log("Hello");
+}
+
+console.log(typeof myFunction);
