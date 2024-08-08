0bb8e90b6a1e929f93bdf4564c1cca04d77f6c35
Yash Tyagi <94339265+itsyashtyagi@users.noreply.github.com>
2024-08-08 04:45:46 +0000

    strings

---
+++ 01_basics/strings.js
@@ -0,0 +1,36 @@
+const firstName = "yash"
+const lastName = "tyagi"
+// console.log(firstName + lastName + " 25 repos"); // Not recommended
+
+// console.log(`My name is ${firstName} ${lastName} and having a 25 repos on github`);
+
+const gameName = new String("yash-tyagi-yt")
+// console.log(gameName.length);
+// console.log(firstName.length);
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('s'));
+// console.log(firstName.charAt(2));
+// console.log(firstName.indexOf('s'));
+
+// console.log(gameName.substring(0, 4));
+// console.log(gameName.slice(-13, 4));
+// console.log(gameName.slice(-10, 4));
+
+const newString = "   yash   "
+// console.log(newString.trim());
+const anotherString = " yash   tyagi   "
+// console.log(anotherString.trim());
+
+const url = "https://yash.com/yash%20tyagi"
+
+console.log(gameName.__proto__);
+console.log(gameName.split("-"));
+console.log(url.replace('%20', '-'));
+
+
+
+
+
+
+
+
