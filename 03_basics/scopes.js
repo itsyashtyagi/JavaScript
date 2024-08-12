72eff10ea2abe4db2f429211d4d193d39f32c607
Yash Tyagi <94339265+itsyashtyagi@users.noreply.github.com>
2024-08-12 05:00:20 +0000

    scope

---
+++ 03_basics/scopes.js
@@ -0,0 +1,39 @@
+if(true){
+   let a= 10
+   const b = 20
+   var c = 30 
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c); // this why we are not using the var keyword
+
+// function one(){
+//     const username = "yash"
+
+//     function two(){
+//         const website = "website"
+//         console.log(username + website);
+//         console.log(website);
+//     }
+//     two()
+//     // console.log(website);
+//     console.log(username);
+// }
+
+// one()
+
+// ++++++++++++++++++ intersting topic +++++++++++++++
+
+// console.log(addOne(5))
+
+function addOne(num1){
+    return num1 + 1;
+}
+
+console.log(result(4))
+const result = function(num2){
+    return num2 + 2;
+}
+
+
