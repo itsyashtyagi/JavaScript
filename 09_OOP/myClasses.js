298781021e4328983673bf5c9ea8b659afcf87b7
Yash Tyagi <94339265+itsyashtyagi@users.noreply.github.com>
2024-09-25 03:49:45 +0000

    classes in JS

---
+++ 09_OOP/myClasses.js
@@ -0,0 +1,43 @@
+// ES6
+
+// class User {
+//     constructor(username, email, password) {
+//         this.username = username;
+//         this.email = email;
+//         this.password = password;
+//     }
+
+//     encryptPassword() {
+//         return `${this.password}abc`;
+//     }
+
+//     changeUsername() {
+//         return `${this.username.toUpperCase()}`;
+//     }
+// }
+
+// const user1 = new User("Yash", "yashtyagi@gmail.com", "1234");
+
+// console.log(user1.encryptPassword());
+// console.log(user1.changeUsername());
+
+// behind the scene
+
+function User(username, email, password) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+}
+
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`;
+}
+
+User.prototype.changeUsername = function(){
+    return `${this.username.toUpperCase()}`;
+}
+
+const user1 = new User("Yash", "yashtyagi@gmail.com", "1234");
+
+console.log(user1.encryptPassword());
+console.log(user1.changeUsername());
